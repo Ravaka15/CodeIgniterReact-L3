f68852098ad654f98b39173c9a3eee63a4ec58e3
ravaka15 <ravakiniainatokyhary@gmail.com>
2022-10-17 15:18:57 +0300

    tuto 3 demi

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -7,10 +7,12 @@ class CustomerController extends Controller
 {
     
     protected $customer;
+    protected $request;
 
     public function __construct()
     {
-        $this->customer = new CustomerModel();
+        $this->client = new CustomerModel();
+        $this->request = \Config\Services::request();
     }
 
     public function index()
@@ -20,7 +22,28 @@ class CustomerController extends Controller
 
     public function test()
     {
-        $data = $this->customer->findAll();
+        $data = $this->client->findAll();
         return json_encode($data);
+    }
+
+    public function create()
+    {
+        try {
+            $json = $this->request->getJSON();
+            $insert['nomClient'] = $json->nomClient;
+            $insert['prenomClient'] = $json->prenomClient;
+            $insert['telephoneClient'] = $json->telephoneClient;
+            $insert['emailClient'] = $json->emailClient;
+            $insert['adresseClient'] = $json->adresseClient;
+            $res = $this->client->insert($insert);
+            $response['success'] = true;
+            $response['message'] = "Bien enregistre";
+
+            return json_encode($response);
+        } catch (\Exception $e) {
+            $response['success'] = false;
+            $response['message'] = $e->getMessage();
+            return json_encode($response);
+        }
     }
 }
