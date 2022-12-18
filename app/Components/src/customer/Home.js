62154d6e60358117eb331d0882bcc4956f468acc
ravaka15 <ravakiniainatokyhary@gmail.com>
2022-12-18 17:43:28 +0300

    ssd

--- app/Components/src/customer/Home.js
+++ app/Components/src/customer/Home.js
@@ -13,21 +13,22 @@ export default class Home extends Component {
         </MDBRow>
 
         <MDBRow style={{textDecoration: 'none', 
-          color: 'rgb(150, 41, 108)',fontWeight: 'bold',fontSize: 25}} 
+          color: 'rgb(172, 108, 147)',fontWeight: 'bold',fontSize: 25}} 
           className={styles.textTitre}>
           <MDBCol lg='8'>
           <p> La societe Lazan'iBetsileo vous presente </p>
           </MDBCol>
+
+
         </MDBRow>
 
         <MDBRow className={styles.text}>
-          <MDBCol lg='7'>
+          <MDBCol lg='8'>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  </p>
           </MDBCol>
     
-          <MDBCol lg='5'>
+          <MDBCol lg='4'>
             <img
               src={home}
               className='img-fluid'
@@ -35,6 +36,11 @@ export default class Home extends Component {
             />
           </MDBCol>
       </MDBRow>
+
+      <MDBRow style={{height: 100}}>
+          <MDBCol  lg='6'>
+          </MDBCol>
+        </MDBRow>
       </>
     )
   }
